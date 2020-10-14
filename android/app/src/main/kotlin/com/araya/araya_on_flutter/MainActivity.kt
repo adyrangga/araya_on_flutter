d136a856b6df2fd38dfbed62623ebfc173004941
ady_rangga <adyranggahidayat@gmail.com>
2020-10-14 23:57:02 +0700

    add dependencies

---
+++ android/app/src/main/kotlin/com/araya/araya_on_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.araya.araya_on_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  Constants._();
+
+  static const String APP_NAME = 'Araya on Flutter';
+}
